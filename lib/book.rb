97548124e643e5b74027d3e30ac03937a7c15331
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-04-05 14:37:42 -0400

    commit tree d96e539622ce03d5cdaca0a2deffc59d58a087b5

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
 
+class Book
+
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+new_book = Book.new("It's my book now")
+new_book.author = "Me"
+new_book.page_count = 100
+new_book.genre = "Erotic Fiction"
+
+
+
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+new_shoe = Shoe.new("Iman Potato")
+new_shoe.color = "brown"
+new_shoe.size = 16
+new_shoe.material = "human skin"
+new_shoe.condition = 10/10
+
+
